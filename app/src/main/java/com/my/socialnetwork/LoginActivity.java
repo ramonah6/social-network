76c03131eb24adfb42e8dbf9c20747fdf627770d
Chong-Gu <chonggu24@gmail.com>
2019-12-17 04:31:23 -0800

    dependence updata

--- app/src/main/java/com/my/socialnetwork/LoginActivity.java
+++ app/src/main/java/com/my/socialnetwork/LoginActivity.java
@@ -3,7 +3,7 @@ package com.my.socialnetwork;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class loginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/my/socialnetwork/MainActivity.java
+++ app/src/main/java/com/my/socialnetwork/MainActivity.java
@@ -1,5 +1,6 @@
 package com.my.socialnetwork;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +13,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,10 +25,14 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView postList;
     private Toolbar mToolbar;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -46,6 +54,23 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+       FirebaseUser currentUser = mAuth.getCurrentUser();
+       if (currentUser == null) {
+            SendUsertoLoginActivity();
+       }
+    }
+
+    private void SendUsertoLoginActivity() {
+        Intent loginIntent =  new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags((Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+        startActivity(loginIntent);
+        finish();
     }
 
     @Override
