681a606dbc206af56d61655ff9d547542bfbc252
ramonah6 <yuanyh68@gmail.com>
2019-12-20 17:03:38 -0800

    add find friends ui activiy

--- app/src/main/java/com/my/socialnetwork/FindFriendsActivity.java
+++ app/src/main/java/com/my/socialnetwork/FindFriendsActivity.java
@@ -1,13 +1,23 @@
 package com.my.socialnetwork;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class FindFriendsActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+        mToolBar = (Toolbar)findViewById(R.id.find_friends_appbar_layout);
+        setSupportActionBar(mToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
     }
 }
