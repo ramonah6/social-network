9af03f7df174182a85748c88a8784cae7adcb314
Chong-Gu <chonggu24@gmail.com>
2019-12-20 19:03:16 -0800

    update

--- app/src/main/java/com/my/socialnetwork/FindFriendsActivity.java
+++ app/src/main/java/com/my/socialnetwork/FindFriendsActivity.java
@@ -1,23 +1,120 @@
 package com.my.socialnetwork;
 
+import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
     private Toolbar mToolBar;
+    private ImageButton SearchButton;
+    private EditText SearchInputText;
+
+    private RecyclerView SearchResultList;
+
+    private DatabaseReference allUsersDatabaseRef;
+    private FirebaseRecyclerAdapter firebaseRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
 
+        allUsersDatabaseRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
         mToolBar = (Toolbar)findViewById(R.id.find_friends_appbar_layout);
         setSupportActionBar(mToolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Update Post");
+
+        SearchResultList = (RecyclerView)findViewById(R.id.search_result_list);
+        SearchResultList.setHasFixedSize(true);
+        SearchResultList.setLayoutManager(new LinearLayoutManager(this));
+
+        SearchButton = (ImageButton)findViewById(R.id.search_people_friends_button);
+        SearchInputText = (EditText)findViewById(R.id.search_box_input);
+
+        SearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String searchBoxInput = SearchInputText.getText().toString();
+
+                SearchPeopleFriends(searchBoxInput);
+            }
+        });
+    }
+
+    private void SearchPeopleFriends(String searchBoxInput) {
+        FirebaseRecyclerOptions<FindFriends> options =
+                new FirebaseRecyclerOptions.Builder<FindFriends>()
+                        .setQuery(allUsersDatabaseRef, FindFriends.class)
+                        .build();
+
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<FindFriends, FindFriendsActivity.FindFriendsViewHolder>(options){
+            @Override
+            protected void onBindViewHolder(@NonNull MainActivity.PostsViewHolder holder, int position, @NonNull Posts model) {
+                final String PostKey = getRef(position).getKey();
+
+                holder.setFullname(model.getFullname());
+                holder.setTime(model.getTime());
+                holder.setDate(model.getDate());
+                holder.setDescription(model.getDescription());
+                holder.setProfileimage(getApplicationContext(), model.getProfileimage());
+                holder.setPostimage(getApplicationContext(), model.getPostimage());
+
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent clickPostIntent = new Intent(FindFriendsActivity.this, ClickPostActivity.class);
+                        clickPostIntent.putExtra("PostKey", PostKey);
+                        startActivity(clickPostIntent);
+                    }
+                });
+            }
+
+            @Override
+            public FindFriendsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                // Create a new instance of the ViewHolder, in this case we are using a custom
+                // layout called R.layout.message for each item
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.all_post_layout, parent, false);
+
+                return new FindFriendsViewHolder(view);
+            }
+        };
+
+        SearchResultList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class FindFriendsViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public FindFriendsViewHolder(View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+        public void setProfileimage(String profileiamge) {
+
+        }
     }
 }
