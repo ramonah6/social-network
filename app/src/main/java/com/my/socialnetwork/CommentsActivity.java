ad90b6ec6119792d5885dfa069ad8696c17ecbba
Chong-Gu <chonggu24@gmail.com>
2019-12-23 17:07:02 -0800

    comment activity

--- app/src/main/java/com/my/socialnetwork/CommentsActivity.java
+++ app/src/main/java/com/my/socialnetwork/CommentsActivity.java
@@ -2,12 +2,37 @@ package com.my.socialnetwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.common.images.internal.PostProcessedResourceCache;
 
 public class CommentsActivity extends AppCompatActivity {
+
+    private ImageButton PostCommentButton;
+    private EditText CommentInputText;
+    private RecyclerView CommentListsts;
+
+    private String Post_Key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        Post_Key = getIntent().getExtras().get("PostKey").toString();
+
+        CommentListsts = (RecyclerView) findViewById(R.id.comment_list);
+        CommentListsts.setHasFixedSize(true);
+        LinearLayoutManager linearlayoutManager = new LinearLayoutManager(this);
+        linearlayoutManager.setReverseLayout(true);
+        linearlayoutManager.setStackFromEnd(true);
+        CommentListsts.setLayoutManager(linearlayoutManager);
+
+        CommentInputText = (EditText) findViewById(R.id.comment_input);
+        PostCommentButton = (ImageButton)findViewById(R.id.post_comment_button);
     }
 }
