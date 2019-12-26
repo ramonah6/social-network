6578005c1cae7cd10032b42a3eca8cffae44b1d1
ramonah6 <yuanyh68@gmail.com>
2019-12-25 17:04:57 -0800

    done my post

--- app/src/main/java/com/my/socialnetwork/MyPostsActivity.java
+++ app/src/main/java/com/my/socialnetwork/MyPostsActivity.java
@@ -1,13 +1,156 @@
 package com.my.socialnetwork;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MyPostsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView myPostList;
+    private FirebaseAuth mAuth;
+    private DatabaseReference PostsRef;
+    private String currentUserID;
+    private FirebaseRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_posts);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        PostsRef = FirebaseDatabase.getInstance().getReference().child("Posts");
+
+        mToolbar = (Toolbar)findViewById(R.id.my_posts_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("My Posts");
+
+
+        myPostList = (RecyclerView) findViewById(R.id.my_all_posts_list);
+        myPostList.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        myPostList.setLayoutManager(linearLayoutManager);
+
+        DisplayMyAllPosts();
+    }
+
+    private void DisplayMyAllPosts() {
+        Query SortPostsInDecendingOrder = PostsRef.orderByChild("uid")
+                .startAt(currentUserID).endAt(currentUserID + "\uf8ff");
+
+        FirebaseRecyclerOptions<Posts> options =
+                new FirebaseRecyclerOptions.Builder<Posts>()
+                        .setQuery(SortPostsInDecendingOrder, Posts.class)
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<Posts,MyPostsViewHolder>(options){
+            @NonNull
+            @Override
+            public MyPostsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.all_post_layout, parent, false);
+
+                return new MyPostsViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MyPostsViewHolder holder, int position, @NonNull Posts model) {
+                holder.setFullname(model.getFullname());
+                holder.setTime(model.getTime());
+                holder.setDate(model.getDate());
+                holder.setDescription(model.getDescription());
+                holder.setProfileimage(getApplicationContext(), model.getProfileimage());
+                holder.setPostimage(getApplicationContext(), model.getPostimage());
+
+            }
+        };
+
+        myPostList.setAdapter(adapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    public static class MyPostsViewHolder extends RecyclerView.ViewHolder
+    {
+        View mView;
+
+        public MyPostsViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+        public void setFullname(String fullname)
+        {
+            TextView username = (TextView) mView.findViewById(R.id.post_user_name);
+            username.setText(fullname);
+        }
+
+        public void setProfileimage(Context ctx, String profileimage)
+        {
+            CircleImageView image = (CircleImageView) mView.findViewById(R.id.post_profile_image);
+            Picasso.with(ctx).load(profileimage).into(image);
+        }
+
+        public void setTime(String time)
+        {
+            TextView PostTime = (TextView) mView.findViewById(R.id.post_time);
+            PostTime.setText("  " + time);
+        }
+
+        public void setDate(String date)
+        {
+            TextView PostDate = (TextView) mView.findViewById(R.id.post_date);
+            PostDate.setText("  " + date);
+        }
+
+        public void setDescription(String description)
+        {
+            TextView PostDescription = (TextView) mView.findViewById(R.id.click_post_description);
+            PostDescription.setText(description);
+        }
+
+        public void setPostimage(Context ctx1, String postimage)
+        {
+            ImageView PostImage = (ImageView) mView.findViewById(R.id.click_post_image);
+            Picasso.with(ctx1).load(postimage).into(PostImage);
+        }
+
     }
 }
